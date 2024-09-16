7bb58af7d5f822b53453826837ca8f64b2147ea5
Elliott Lewandowski <elewandowski30@gmail.com>
2024-09-16 09:55:14 -0800

    Got normal vector calculations working properly

--- scripts/sun.gd
+++ scripts/sun.gd
@@ -1,5 +1,7 @@
 extends OmniLight3D
 
+@export var radius : float = 2.0;
+
 var wave : float = 0.0;
 
 # Called when the node enters the scene tree for the first time.
@@ -10,4 +12,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	wave += delta;
-	position = Vector3(sin(wave) * 2.0, position.y, cos(wave) * 2.0);
+	position = Vector3(sin(wave) * radius, position.y, cos(wave) * radius);
